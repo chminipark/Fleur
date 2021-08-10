b5e167262c8fce815ea80a5e025eb3013d664db1
chminipark <chminipark@gmail.com>
2021-08-10 21:39:02 +0900

    First Commit

---
+++ Fleur/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fleur
+//
+//  Created by minii on 2021/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Fleur/FleurApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleurApp.swift
+//  Fleur
+//
+//  Created by minii on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct FleurApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
