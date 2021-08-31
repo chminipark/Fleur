b0ef1459c56cca106474919f1b6cb561ca14e410
chminipark <chminipark@gmail.com>
2021-08-31 14:59:43 +0900

    Text Shimmer animation

--- Fleur/Views/HashTagView.swift
+++ Fleur/Views/HashTagView.swift
@@ -120,7 +120,7 @@ struct HashTagView: View {
           ForEach(searchText.isEmpty ? data : data.filter({($0.lowercased()).contains(self.searchText.lowercased())}),
                   id: \.self,
                   content: { data in
-            HashTagText(text: data)
+                    HashTagText(text: data, animatedText: searchText)
           })
           
         }
@@ -142,6 +142,8 @@ struct HashTagText: View {
   
   var text: String
   
+  var animatedText: String
+  
   @State var animation = false
   
   var body: some View {
@@ -153,39 +155,40 @@ struct HashTagText: View {
 //        .foregroundColor(Color.white.opacity(0.5))
       
       // MultiColor Text
-      HStack(spacing:0) {
-        ForEach(0..<text.count, id: \.self, content: { index in
-          Text(String(text[text.index(text.startIndex, offsetBy: index)]))
-            .font(.system(size: 50, weight: .semibold))
-            .foregroundColor(randomColor())
+      if !animatedText.isEmpty {
+        HStack(spacing:0) {
+          ForEach(0..<text.count, id: \.self, content: { index in
+            Text(String(text[text.index(text.startIndex, offsetBy: index)]))
+              .font(.system(size: 50, weight: .semibold))
+              .foregroundColor(searchTextIndex(sentence: text, search: animatedText).contains(index) ? randomColor() : Color.black)
+              
+          })
+        }
+        .mask(
+          
+          Rectangle()
             
+            // 색상 그라데이션
+            .fill(
+              
+              LinearGradient(gradient: .init(colors: [Color.black.opacity(1)]), startPoint: .top, endPoint: .bottom)
+            )
+            
+            .rotationEffect(.init(degrees: 70))
+            .padding(20)
+            
+            .offset(x: -250)
+            .offset(x: animation ? 500 : 0)
+          
+        )
+        .onAppear(perform: {
+          
+          withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: false)) {
+            animation.toggle()
+          }
+          
         })
       }
-      .mask(
-        
-        Rectangle()
-          
-          // 색상 그라데이션
-          .fill(
-          
-            LinearGradient(gradient: .init(colors: [Color.white.opacity(1)]), startPoint: .top, endPoint: .bottom)
-          )
-          
-          .rotationEffect(.init(degrees: 70))
-          .padding(20)
-        
-          .offset(x: -250)
-          .offset(x: animation ? 500 : 0)
-      
-      )
-      .onAppear(perform: {
-        
-        withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
-        animation.toggle()
-        }
-        
-      })
-      
       
     }
   }
@@ -194,6 +197,30 @@ struct HashTagText: View {
     let color = UIColor(red: CGFloat.random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
     return Color(color)
   }
+  
+  func searchTextIndex(sentence: String, search searchText: String) -> [Int] {
+    
+    var sentence = sentence
+    var start = 0
+    var cnt = 1
+    var indexS = [Int]()
+    
+    while let range = sentence.range(of: searchText, options: .caseInsensitive) {
+      start += sentence.distance(from: sentence.startIndex, to: range.lowerBound)
+      indexS.append(start)
+      while cnt != searchText.count {
+        let index = start + cnt
+        indexS.append(index)
+        cnt += 1
+      }
+      cnt = 1
+      start += sentence.distance(from: sentence.startIndex, to: range.upperBound) - 1
+      sentence = String(sentence[sentence.index(range.upperBound, offsetBy: 0)...])
+    }
+    
+    return indexS
+  }
+
   
 }
 
